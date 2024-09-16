b51c1f6a4b6873242d6ea10e41f105d7f8e73a94
root <root@Kerry>
2024-09-17 00:11:39 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+// Define the companyName as a constant
+const companyName = 'Scuber';
+
+// Define the mostProfitableNeighborhood using let
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Define the companyCeo using let
+let companyCeo = 'Susan Smith';
